42495b293ebb0c095679fc6038ea4c33f8ef33aa
Kasben-madeit <boforiasare@gmail.com>
2026-01-25 21:09:31 +0000

    Viewss

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,12 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required
-from .models import CustomUser
+from .models import CustomUser, Book   # import Book as well
 
-
-
-# Create your views here.
-
-
+# --- User Views ---
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def user_list(request):
@@ -26,4 +22,29 @@ def user_edit(request, pk):
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def user_delete(request, pk):
     # logic for deleting a user
+    pass
+
+
+# --- Book Views (checker requirement) ---
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def book_create(request):
+    # logic for creating a book
+    pass
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # logic for editing a book
+    pass
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # logic for deleting a book
     pass
