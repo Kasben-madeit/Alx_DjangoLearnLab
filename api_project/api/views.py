da63fc4139b771b087e3bdbc980b8a779ad904db
Kasben-madeit <boforiasare@gmail.com>
2026-02-01 22:41:49 +0000

    CRUD

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets
+
 
 
 
@@ -12,3 +14,10 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()       # all books from the database
     serializer_class = BookSerializer   # use the serializer to format output
 
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for viewing and editing Book instances.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
